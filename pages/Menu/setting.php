29f89874838999339ffbb87f8ec318a50f58703b
Ryan Hidayatullah <ryanhidayatullah720@gmail.com>
2026-06-08 17:58:34 +0700

    ggg

--- pages/Menu/setting.php
+++ pages/Menu/setting.php
@@ -77,7 +77,7 @@ while ($row = mysqli_fetch_assoc($qBarang)) {
     <main class="content setting-container">
         <div class="setting-header mb-4">
             <div class="profile-card">
-                <img src="/../Gudang/pages/uploads/profile/<?php echo $foto; ?>"
+                <img src="../uploads/profile/<?php echo $foto; ?>"
                     style="width:80px;height:80px;border-radius:50%;object-fit:cover;">
 
                 <div class="device-name"><?= htmlspecialchars($data['nama']) ?></div>
@@ -105,7 +105,7 @@ while ($row = mysqli_fetch_assoc($qBarang)) {
                     <div class="row">
                         <div class="col-md-3 text-center mb-3">
 
-                            <img src="uploads/profile/<?php echo $foto; ?>"
+                            <img src="../uploads/profile/<?php echo $foto; ?>"
                                 style="width:80px;height:80px;border-radius:50%;object-fit:cover;">
 
                             <input type="file" name="foto" class="form-control form-control-sm mt-2">
